99265969061f8dfcf74d2816d39baab153bbf74b
vinton <vinton.mail@gmail.com>
2020-11-12 19:09:25 +0300

    conscience: finish person page, update variables, data files, add media img, update store

--- pages/person/_id.vue
+++ pages/person/_id.vue
@@ -4,12 +4,73 @@
     <nuxt-link to="/search" class="page__link page__link--back">
       Назад к поиску
     </nuxt-link>
-    <h1>Личное дело №{{ id }}</h1>
+    <template v-if="currentPerson">
+      <!-- <h1>Личное дело №{{ personalNumber }}: {{ currentPerson.name }}</h1>
+      <p>{{ currentPerson.bio }}</p> -->
+      <section class="person__profile profile">
+        <div class="profile__col profile__col--first">
+          <h1 class="profile__name">{{ currentPerson.name }}</h1>
+          <p class="profile__bio">{{ currentPerson.bio }}</p>
+        </div>
+        <div class="profile__col profile__col--last">
+          <ul class="profile__info">
+            <li class="profile__info-part">
+              Год рождения: {{ currentPerson.birthday }}
+            </li>
+            <li class="profile__info-part">
+              Дата смерти: {{ currentPersonDeathDate }}
+            </li>
+          </ul>
+          <img
+            :src="`/media/${currentPerson.img}`"
+            :alt="currentPerson.title"
+            class="profile__img img-responsive"
+          />
+        </div>
+      </section>
+    </template>
+    <template v-else>
+      <h1>Личное дело №0: неизвестный</h1>
+      <p>Неизвестный погибший...</p>
+    </template>
   </main>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex'
+
 export default {
+  // asyncData({route, store, params, query) {
+  //   const loadedVictim = await this.$content('victims')
+  //     .where
+  // },
+  async fetch() {
+    console.log('fetch called!')
+    if (!this.currentPerson) {
+      console.log(
+        `no person data loaded, making an API call where ID: ${this.id}`
+      )
+      const loadedVictims = await this.$content('victims')
+        // .where({ id: { $eq: this.id } })
+        .where({ id: this.id })
+        .fetch()
+      console.log('loaded victim in arr:', loadedVictims)
+      if (loadedVictims.length) {
+        const victimObj = loadedVictims[0]
+        console.log('victimObj:', victimObj)
+        const victimGroup = victimObj.name.charAt(0).toLowerCase()
+        console.log('victimGroup:', victimGroup)
+        if (!this.victims[victimGroup])
+          this.setVictimsGroup([victimGroup, [victimObj]])
+        else this.setVictim([victimGroup, victimObj])
+        // MEMO: load all group in case going back to search!
+        const allVictimsGroup = await this.$content('victims')
+          .where({ slug: victimGroup })
+          .fetch()
+        this.setVictimsGroup([victimGroup, allVictimsGroup])
+      } else console.warn('Victim not found in API!')
+    }
+  },
   data() {
     return {
       victim: {
@@ -23,8 +84,27 @@ export default {
     }
   },
   computed: {
+    ...mapState('victims', ['victims']),
+    allVictims() {
+      return this.victims.all
+    },
     id() {
-      return this.$route.params.id
+      return parseInt(this.$route.params.id)
+    },
+    personalNumber() {
+      return this.id + 1
+    },
+    currentPerson() {
+      return this.allVictims.find((v) => v.id === this.id)
+    },
+    currentPersonDeathDate() {
+      return this.currentPerson.death
+        .split('/')
+        .map((d, i, arr) => {
+          if (i === arr.length - 1) return '19' + d
+          return d
+        })
+        .join('.')
     },
   },
   mounted() {
@@ -35,16 +115,24 @@ export default {
   },
   head() {
     return {
-      title: 'личное дело #' + this.id,
+      title: 'личное дело #' + this.personalNumber,
       meta: [
         {
           hid: 'description',
           name: 'description',
-          content: 'Личное дело #' + this.id,
+          content: 'Личное дело #' + this.personalNumber,
         },
       ],
     }
   },
+  methods: {
+    ...mapActions('victims', ['setVictim', 'setVictimsGroup']),
+  },
+  // beforeRouteLeave(to, from, next) {
+  //   if (to.name === 'search') {
+  //     this.content
+  //   }
+  // },
 }
 </script>
 
@@ -54,4 +142,55 @@ export default {
 
   .person
     background-color: $black
+
+  .person__profile // section.profile
+    //
+
+  .profile // section.person__profile
+    display: flex
+    flex-flow: row wrap
+    align-items: flex-start
+    justify-content: space-between
+    width: 100%
+    // margin-left: - $unitX1_5
+    // margin-right: - $unitX1_5
+
+  .profile__col
+    display: flex
+    flex-flow: column wrap
+    flex-grow: 1
+    flex-basis: 50%
+    align-items: stretch
+    width: 50%
+    padding: $unitX1_5
+
+  .profile__col--first
+    padding-left: 0
+
+  .profile__col--last
+    padding-right: 0
+
+  .profile__name // h1
+    font-family: $f_garamond
+    font-size: 3.75rem // 60px
+    line-height: 1
+    margin: 0
+    margin-bottom: $unitX2
+
+  .profile__bio
+    font-family: $f_garamond
+    font-size: 1.875rem // 30px
+    line-height: 1.2    // 36px
+    flex-basis: 75%
+
+  .profile__info // ul
+    font-family: $f_mono
+    font-size: 1.875rem // 30px
+    line-height: 1.2    // 36px
+    list-style: none
+    text-align: right
+    margin-bottom: $unitX3
+
+  .profile__info-part
+    //
 </style>
--- store/victims.js
+++ store/victims.js
@@ -1,24 +1,40 @@
 import Vue from 'vue'
 
 export const state = () => ({
-  victims: {},
+  victims: {
+    all: [],
+  },
 })
 export const mutations = {
-  SET_VICTIMS(state, victims) {
-    state.victims = victims
+  SET_ALL_VICTIMS(state, victims) {
+    state.victims.all = victims
+  },
+  UPDATE_ALL_VICTIMS(state, victims) {
+    console.log(`M: adding victims ${victims} to 'all' group:`)
+    state.victims.all.push(...victims)
   },
   SET_VICTIMS_GROUP(state, [group, victims]) {
-    console.log(`M: setting victims group ${group} to ${victims}`)
+    console.log(`M: setting victims group '${group}' to ${victims}`)
     // state.victims[group] = victims
     Vue.set(state.victims, group, victims)
   },
+  SET_VICTIM(state, [group, victim]) {
+    console.log(`M: setting victim ${victim} to group '${group}'`)
+    state.victims[group].push(victim)
+  },
 }
 export const actions = {
-  setVictims({ commit }, victims) {
-    commit('SET_VICTIMS', victims)
+  setAllVictims({ commit }, victims) {
+    commit('SET_ALL_VICTIMS', victims)
   },
-  setVictimsGroup({ commit }, groupAndVictims) {
-    console.log('A: setting victims group with:', groupAndVictims)
-    commit('SET_VICTIMS_GROUP', groupAndVictims)
+  setVictimsGroup({ commit }, [group, victims]) {
+    console.log(`A: setting group '${group}' with victims ${victims}`)
+    commit('SET_VICTIMS_GROUP', [group, victims])
+    commit('UPDATE_ALL_VICTIMS', victims)
+  },
+  setVictim({ commit }, [group, victim]) {
+    console.log(`A: setting victim ${victim} to group '${group}'`)
+    commit('SET_VICTIM', [group, victim])
+    commit('UPDATE_ALL_VICTIMS', [victim])
   },
 }
