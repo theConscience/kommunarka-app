f5468b241fd418b7151805484797aed93aff7dd1
vinton <vinton.mail@gmail.com>
2020-12-01 01:07:02 +0300

    6aaf106

---
+++ _nuxt/static/1606774016/person/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1606774016",layout:"default",error:null,state:{idle_time:a,search:{searchActive:false,searchQuery:"",filterKeyId:a},victims:{victims:{all:[]}}},serverRendered:true,routePath:"\u002Fperson",config:{content:{dbHash:"ca2e8a96"}}}}(0));
