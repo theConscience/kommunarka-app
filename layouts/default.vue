022d15949bf15a4bba28f18daaa826372a0e0675
vinton <vinton.mail@gmail.com>
2020-12-01 01:05:14 +0300

    conscience: add idle reset after 5 mins

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,22 +9,56 @@
 
 <script>
 /* eslint-disable no-console */
+import { mapActions } from 'vuex'
 import NavBar from '~/components/NavBar.vue'
 export default {
+  // MEMO: next running on Server side, and though document is not available
+  //       rewrite with window.onNuxtready
+  // middleware({ store, redirect }) {
+  //   const idleTime = 1 * 60 * 1000
+  //   store.dispatch('onIdle', [
+  //     idleTime,
+  //     () => {
+  //       store.dispatch('search/disableSearch')
+  //       store.dispatch('search/resetSearchQuery')
+  //       store.dispatch('search/resetFilterKeyId')
+  //       redirect('/search')
+  //     },
+  //   ])
+  // },
   components: {
     NavBar,
   },
   mounted() {
     document.addEventListener('contextmenu', this.onContextMenuAction)
+    window.onNuxtReady(() => this.resetIdle())
   },
   beforeDestroy() {
     document.removeEventListener('contextmenu', this.onContextMenuAction)
   },
   methods: {
+    ...mapActions(['onIdle']),
+    ...mapActions('search', [
+      'disableSearch',
+      'resetSearchQuery',
+      'resetFilterKeyId',
+    ]),
     // DISABLE RIGHT CLICK CONTEXT MENU
     onContextMenuAction(evt) {
       console.warn('User tried to open context menu')
       evt.preventDefault()
+    },
+    resetIdle(idleTime = 5 * 60 * 1000) {
+      // console.log('resetIdle in 5 mins!')
+      // 5 mins
+      this.onIdle([idleTime, () => this.reset()])
+    },
+    reset() {
+      // console.log('Reset by timer!')
+      this.disableSearch()
+      this.resetSearchQuery()
+      this.resetFilterKeyId()
+      window.$nuxt.$router.push('/Search')
     },
   },
 }
---
+++ store/index.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-console */
+export const state = {
+  idle_time: 0,
+}
+
+export const mutations = {
+  UPDATE_IDLE_TIME(state, newIdleTime) {
+    state.idle_time = newIdleTime
+  },
+}
+
+export const actions = {
+  onIdle({ state, commit }, [cbIdleTime, cb]) {
+    console.log('▲ ACTION (sync): dispatching on_idle')
+
+    setInterval(() => timerIncrement(), 60 * 1000)
+
+    if (process.client) {
+      console.log('Is Client!')
+      document.addEventListener('mousemove', () =>
+        commit('UPDATE_IDLE_TIME', 0)
+      )
+      document.addEventListener('keydown', () => commit('UPDATE_IDLE_TIME', 0))
+    } else {
+      console.log('is server! no Idle reset set')
+    }
+
+    function timerIncrement() {
+      commit('UPDATE_IDLE_TIME', state.idle_time + 1)
+      const minutesBeforeReset = cbIdleTime / (60 * 1000)
+      // console.log('idle time is over', state.idle_time, 'minute, call cb() in over', minutes_before_reset, 'minutes')
+      if (state.idle_time >= minutesBeforeReset) {
+        // minutes
+        // console.log('Going to call cb() in on_idle timerIncrement() action')
+        cb()
+      }
+    }
+  },
+}
